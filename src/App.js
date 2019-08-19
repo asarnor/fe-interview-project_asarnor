f30bb1d79448beab4386e0b5cc579d0b18565a9a
abdul sarnor <teamcodebehind@hotmail.com>
2019-08-19 04:28:48 -0700

    used createjs to generate gfx instead of svg elements

--- src/App.js
+++ src/App.js
@@ -2,20 +2,141 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 import CanvasDropdownContainer from "./DropdownSelector/CanvasDropdownContainer";
 import ButtonListContainer from "./ButtonList/ButtonListContainer";
-import CanvasContainer from "./Canvas/Symbols/CanvasContainer";
 import fetchData from "./data/fetchData";
-
-/**
- * getData gets our data
- */
-const getData = () => fetchData().then(x => x);
+import { Stage, Shape, Container, Shadow } from "@createjs/easeljs";
 
 const App = () => {
+  /** State */
   const [data, setData] = useState({});
   const [dropdownItems, setDropdownItems] = useState([]);
   const [contentsList, setContentsList] = useState([]);
-  const [selectedSymbol, setSymbol] = useState("");
+  const [selectedSymbol, setSelectedSymbol] = useState("");
   const [selectedHighlight, setHighlight] = useState(null);
+  const [_stage, setStage] = useState(null);
+
+  /**
+   * getData gets our data
+   */
+  const getData = () => fetchData().then(x => x);
+
+  /**
+   * Reference canvas element for createjs to work
+   */
+  const stage = new Stage("myCanvas");
+
+  /**
+   * drawSymbol is used to handle root symbols and adds them to the canvas
+   * difference between this and nestedSymbol is that this doesnt return a container
+   * @param {string} reference 
+   */
+  const drawSymbol = reference => {
+    const container = new Container();
+    data[reference].contents.forEach(ele => {
+      const shape = new Shape();
+      shape.name = ele.type;
+      switch (ele.type) {
+        case "rect":
+          shape.graphics
+            .beginFill(ele.fill)
+            .drawRect(ele.x, ele.y, ele.width, ele.height)
+            .endFill();
+          container.addChild(shape);
+          break;
+
+        case "oval":
+          const radius = ele.width / 2;
+          shape.graphics
+            .beginFill(ele.fill)
+            .drawCircle(ele.x + radius, ele.y + radius, radius)
+            .endFill();
+          container.addChild(shape);
+          break;
+
+        default:
+          container.addChild(drawCustomSymbol(ele.type, ele.scale, ele.x, ele.y));
+          break;
+      }
+      container.name = ele.type;
+    });
+    stage.addChild(container);
+  };
+
+  /**
+   * drawCustomSymbol is used to handle nested symbols
+   * it returns a container that contains nested symbols
+   * @param {string} reference 
+   * @param {number} scale 
+   * @param {number} x 
+   * @param {number} y 
+   */
+  const drawCustomSymbol = (reference, scale = 1, x = 0, y = 0) => {
+    const container = new Container();
+    data[reference].contents.forEach(ele => {
+      const shape = new Shape();
+      shape.name = ele.type;
+      switch (ele.type) {
+        case "rect":
+          shape.graphics
+            .beginFill(ele.fill)
+            .drawRect(ele.x, ele.y, ele.width, ele.height)
+            .endFill();
+          container.addChild(shape);
+          break;
+
+        case "oval":
+          const radius = ele.width / 2;
+          shape.graphics
+            .beginFill(ele.fill)
+            .drawCircle(ele.x + radius, ele.y + radius, radius)
+            .endFill();
+          container.addChild(shape);
+          break;
+
+        default:
+          container.addChild(drawCustomSymbol(ele.type, ele.scale, ele.x, ele.y));
+          break;
+      }
+    });
+    container.name = reference;
+    container.x = x;
+    container.y = y;
+    container.scale = scale;
+    return container;
+  };
+
+  /**
+   * setCanvasWidthHeight sets the width and height of canvas dynamically
+   * before it renders
+   * @param {string} reference 
+   */
+  const setCanvasWidthHeight = reference => {
+    const canvas = document.getElementById("myCanvas");
+    canvas.width = data[reference].width;
+    canvas.height = data[reference].height;
+  };
+
+
+  /**
+   * highlightSymbol is used to capture the index of
+   * the selected symbol from the list and highlight it
+   * on the canvas
+   * @param {number} idx 
+   */
+  const highlightSymbol = idx => {
+    /**
+     * we first need to reset all highlighted symbols
+     */
+    _stage.children[0].children.forEach(ele => {
+      ele.shadow = null;
+      ele.alpha = 1;
+    });
+
+    /**
+     * identify and highlight the selected symbol
+     */
+    _stage.children[0].getChildAt(idx).shadow = new Shadow("pink", 5, 5, 5);
+    _stage.children[0].getChildAt(idx).alpha = 0.5;
+  };
 
   useEffect(() => {
     /** async call to request our data */
@@ -32,52 +153,54 @@ const App = () => {
   }, []);
 
   /**
-   * 
-   * @param {string} _symbol 
+   *
+   * @param {string} _symbol
    */
   const symbolSetter = _symbol => {
     /** set symbol */
-    setSymbol(_symbol);
+    setSelectedSymbol(_symbol);
     /** reset highlight */
     setHighlight(null);
+    stage.clear();
     /** get symbol's contents using selected */
     setContentsList(data[_symbol].contents.map(ele => ele.type));
+    /** set the proper width and height to canvas area */
+    setCanvasWidthHeight(_symbol);
+    /** draw symbols on canvas */
+    drawSymbol(_symbol);
+    /** update the stage */
+    stage.update();
+    /** set stage object for createjs */
+    setStage(stage);
   };
 
   /**
-   * 
-   * @param {number} index 
+   *
+   * @param {number} index
    */
   const highlightHandler = index => {
+    highlightSymbol(index);
     setHighlight(index);
+    _stage.update();
   };
 
   return (
     <div className="App">
-      <React.Suspense fallback={<div>Loading...</div>}>
-        <header className="App-header">
-          <CanvasDropdownContainer
-            dropData={dropdownItems}
-            setSymbol={symbolSetter}
-          />
-          <ButtonListContainer
-            listData={contentsList}
-            clickHandler={highlightHandler}
-            selectedId={selectedHighlight}
-          />
-        </header>
-        <section>
-          {data && selectedSymbol ? (
-            <CanvasContainer
-              content={data}
-              reference={selectedSymbol}
-              selectedId={selectedHighlight}
-            />
-          ) : (
-            <div>No symbol selected...</div>
-          )}
-        </section>
-      </React.Suspense>
+      <header className="App-header">
+        <CanvasDropdownContainer
+          dropData={dropdownItems}
+          setSymbol={symbolSetter}
+        />
+        <ButtonListContainer
+          listData={contentsList}
+          clickHandler={highlightHandler}
+          selectedId={selectedHighlight}
+        />
+      </header>
+      <section>
+        {(!data || selectedSymbol === "") && <div>No symbol selected...</div>}
+        <canvas id="myCanvas" />
+      </section>
     </div>
   );
 };
